968077bf129aeaba62178425d7259ff1a75a97b5
unsersea <undersea250@gmail.com>
2024-02-15 18:25:43 +0700

    Update

--- normal-pattern/views/includes/ajax/ajax_tour.php
+++ normal-pattern/views/includes/ajax/ajax_tour.php
@@ -215,6 +215,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $previous_link = '';
         $next_link = '';
         $page_link = '';
+        $page_array = [];
 
         //echo $total_links;
 
@@ -253,48 +254,48 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             }
         }
 
-        // for ($count = 0; $count < count($page_array); $count++) {
-        //     if ($page == $page_array[$count]) {
-        //         $page_link .= '
-        //     <li class="page-item active">
-        //     <a class="page-link" href="#">' . $page_array[$count] . ' <span class="sr-only">(current)</span></a>
-        //     </li>
-        //     ';
+        for ($count = 0; $count < count($page_array); $count++) {
+            if ($page == $page_array[$count]) {
+                $page_link .= '
+            <li class="page-item active">
+            <a class="page-link" href="#">' . $page_array[$count] . ' <span class="sr-only">(current)</span></a>
+            </li>
+            ';
 
-        //         $previous_id = $page_array[$count] - 1;
-        //         if ($previous_id > 0) {
-        //             $previous_link = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $previous_id . '"><i class="bx bx-chevron-left"></i></a></li>';
-        //         } else {
-        //             $previous_link = '
-        //     <li class="page-item disabled">
-        //         <a class="page-link" href="#"><i class="bx bx-chevron-left"></i></a>
-        //     </li>
-        //     ';
-        //         }
-        //         $next_id = $page_array[$count] + 1;
-        //         if ($next_id >= $total_links) {
-        //             $next_link = '
-        //     <li class="page-item disabled">
-        //         <a class="page-link" href="#"><i class="bx bx-chevron-right"></i></a>
-        //     </li>
-        //         ';
-        //         } else {
-        //             $next_link = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $next_id . '"><i class="bx bx-chevron-right"></i></a></li>';
-        //         }
-        //     } else {
-        //         if ($page_array[$count] == '...') {
-        //             $page_link .= '
-        //     <li class="page-item disabled">
-        //         <a class="page-link" href="#">...</a>
-        //     </li>
-        //     ';
-        //         } else {
-        //             $page_link .= '
-        //     <li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $page_array[$count] . '">' . $page_array[$count] . '</a></li>
-        //     ';
-        //         }
-        //     }
-        // }
+                $previous_id = $page_array[$count] - 1;
+                if ($previous_id > 0) {
+                    $previous_link = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $previous_id . '"><i class="bx bx-chevron-left"></i></a></li>';
+                } else {
+                    $previous_link = '
+            <li class="page-item disabled">
+                <a class="page-link" href="#"><i class="bx bx-chevron-left"></i></a>
+            </li>
+            ';
+                }
+                $next_id = $page_array[$count] + 1;
+                if ($next_id >= $total_links) {
+                    $next_link = '
+            <li class="page-item disabled">
+                <a class="page-link" href="#"><i class="bx bx-chevron-right"></i></a>
+            </li>
+                ';
+                } else {
+                    $next_link = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $next_id . '"><i class="bx bx-chevron-right"></i></a></li>';
+                }
+            } else {
+                if ($page_array[$count] == '...') {
+                    $page_link .= '
+            <li class="page-item disabled">
+                <a class="page-link" href="#">...</a>
+            </li>
+            ';
+                } else {
+                    $page_link .= '
+            <li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $page_array[$count] . '">' . $page_array[$count] . '</a></li>
+            ';
+                }
+            }
+        }
 
         $output .= $previous_link . $page_link . $next_link;
 
